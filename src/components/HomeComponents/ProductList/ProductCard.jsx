b2a87f897a26fdeddd67f968f59dddd2275aeb10
Asif Mia <asifmia.dev26@gmail.com>
2025-11-11 15:42:58 +0600

    Update prodactcard

--- src/components/HomeComponents/ProductList/ProductCard.jsx
+++ src/components/HomeComponents/ProductList/ProductCard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import fetchProducts from './AllProducts'
 import Filter from './filter';
+import {Button} from 'primereact/button';
 
 import {Image} from 'primereact/image';
 
@@ -33,7 +34,7 @@ const ProductCard = () => {
       const Keys = Object.keys(Products);
       const Values = Keys.map((key) => Products[key]).flat();
       setAllProducts(Values);
-      setProducts(Values);
+      setProducts(Values);ch
 
     };
     getProducts();
@@ -50,17 +51,26 @@ const ProductCard = () => {
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 '>
         {
           products.map((item) =>(
-            <div key={item.id} className="border p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-300">
-              <h1>
-                {item.Name}
-              </h1>
+            <div key={item.id} className="flex flex-col border p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-300">
               <Image
                 src={item.Image}
                 alt={item.Name}
-                width="100%"
-                height="200px"
-                className="object-contain mb-4"
+                width="60%"
+                height="100px"
+                className="object-contain mb-4 flex justify-center "
               />
+              <h1 className='text-lg font-semibold'>
+                {item.Name}
+              </h1>
+              <p className="text-sm mt-2">
+                {item.Description}
+              </p>
+              <div className='mt-auto py-2 flex flex-row justify-between items-center'>
+                <span className="text-xl font-bold text-blue-600">
+                  {item.Price} ৳
+                </span>
+                <Button label="Add to Cart" className="ml-4 bg-green-600 text-white p-button-sm hover:bg-green-700"/>
+              </div>
               
             </div>
           ))
@@ -70,4 +80,4 @@ const ProductCard = () => {
   )
 }
 
-export default ProductCard
+export default ProductCard;
